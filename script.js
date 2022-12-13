7f49ae67571f7f7cc396b50c0270377d35d1e53d
iSyed0 <87315284+iSyed0@users.noreply.github.com>
2022-12-13 19:51:36 +0500

    added comments for referencing

--- script.js
+++ script.js
@@ -35,35 +35,3 @@ function myFunction() {
     });
   }
 
-  /**IMAGE ENLARGE*/
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var cars = document.getElementsByClassName("cars");
-var myModalImg = document.getElementById("myModalImg");
-var myModalImgCaptionText = document.getElementById("myModalImgCaption");
-
-document.body.onclick = function(e) {
-    var clickedEl = window.event ? event.srcElement : e.target;
-    while (clickedEl != null) {
-        if (clickedEl.className && (" " + clickedEl.className + " ").indexOf("cars") != -1) {
-            modal.style.display = "block";
-            myModalImg.src = clickedEl.src;
-            myModalImgCaptionText.innerHTML = clickedEl.alt;
-            return;
-        }
-        clickedEl = clickedEl.parentNode;
-    }
-}
-
-// Get the <span> element that closes the modal
-var closeMyModal = document.getElementsByClassName("closeMyModal")[0];
-
-// When the user clicks on <span> (x), close the modal
-closeMyModal.onclick = function() { 
-  modal.style.display = "none";
-}
-
-
